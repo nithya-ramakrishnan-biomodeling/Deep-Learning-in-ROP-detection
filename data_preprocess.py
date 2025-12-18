09bdfbed86919d90bac76701ab2aa4567e00c6d3
Shebin C Zacharia <123236300+ShebinCZacharia@users.noreply.github.com>
2025-12-18 17:09:26 +0530

    Add Gaussian noise to preprocessing transforms

--- data_preprocess.py
+++ data_preprocess.py
@@ -2,6 +2,7 @@ from torchvision.transforms import v2
 import cv2
 import torch
 import numpy as np
+from torchvision.transforms import functional
 
 
 class Preprocessor:
@@ -23,13 +24,14 @@ class Preprocessor:
         transform = v2.Compose([
         v2.Resize((512, 512)), 
         v2.Lambda(lambda img: img.convert("RGB")),  
-        v2.Lambda(lambda img: cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)), # Since opencv expects BGR format
+        v2.Lambda(lambda img: cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)), 
         v2.Lambda(lambda img: cv2.split(img)[1]), # Extracting the green channel
         v2.Lambda(lambda img: cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)).apply(img)),  
         v2.Lambda(lambda img: torch.tensor(img, dtype=torch.float32).unsqueeze(0)/255.0), # Adding a channel dimension 
         v2.Lambda(lambda img: img.expand(3, -1, -1)),  # Expanding to 3 channels
         v2.RandomCrop((256, 256)), 
         v2.RandomRotation((0, 360)),
+        v2.GaussianNoise(mean=0.0, sigma=0.05, clip=True),
         v2.Normalize((0.456, 0.456, 0.456), (0.224, 0.224, 0.224)),])
         return transform
     
@@ -47,7 +49,7 @@ class Preprocessor:
         v2.Normalize((0.456, 0.456, 0.456), (0.224, 0.224, 0.224)),])
         return transform
     
-    def test_transform(self):
+    def transform(self):
         """Transform which only does preprocessing"""
         transform = v2.Compose([
         v2.Resize((256, 256)),
